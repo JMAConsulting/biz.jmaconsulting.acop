88bee4ec0c4db44e0772f91739eaa9a021e5086b
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-09-18 07:41:46 +0000

    Ensure we only move fields once the document is loaded and fix javascript function to be used

--- js/acop_membership_form.js
+++ js/acop_membership_form.js
@@ -1,4 +1,8 @@
 (function($) {
-  $('#membership').append($('.crm-contribution-main-form-block'));
-  $('#crm-submit-buttons').append($('.crm-contribution-main-form-block'));
+  $(document).ready(function() {
+    $('#membership').appendTo($('.crm-contribution-main-form-block'));
+    $('.payment_options-group').appendTo($('.crm-contribution-main-form-block'));
+    $('#billing-payment-block').appendTo($('.crm-contribution-main-form-block'));
+    $('#crm-submit-buttons').appendTo($('.crm-contribution-main-form-block'));
+  });
 })(CRM.$);
