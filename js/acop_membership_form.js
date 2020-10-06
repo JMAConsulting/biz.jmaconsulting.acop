13ef4c9698b5dbc6229462e2243506a8cddde778
Seamus lee <seamus.lee@jmaconsulting.biz>
2020-10-06 01:55:15 -0400

    Ensure that user is taken back to the Membership fees section if a discount is applied

--- js/acop_membership_form.js
+++ js/acop_membership_form.js
@@ -4,5 +4,8 @@
     $('.payment_options-group').appendTo($('.crm-contribution-main-form-block'));
     $('#billing-payment-block').appendTo($('.crm-contribution-main-form-block'));
     $('#crm-submit-buttons').appendTo($('.crm-contribution-main-form-block'));
+    if ($('#discountcode').length > 0 && $('#discountcode').val() !== '') {
+      $('#membership')[0].scrollIntoView(true);
+    }
   });
 })(CRM.$);
